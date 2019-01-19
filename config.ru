d543c3dede6245e076fda72281430b37b00182e6
baskinbin <sungbin203@naver.com>
2019-01-19 13:31:12 +0900

    init

---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'sinatra'
+require './vct.rb'
+require './test/dd.rb'
+
+#ddddddddd .은 현재 ..은 그 위에
+run Sinatra::Application
---
+++ vct.rb
@@ -0,0 +1,24 @@
+get '/' do
+    puts 123.to_s
+    "Hello World"
+  end
+
+ get '/baskinbin' do
+    puts "get 들어왔습니다."
+    @a=80
+    if @a>=90 
+        @p= "수"
+    elsif @a>=80 
+        @p= "우"
+    elsif @a>=70 
+        @p= "미"
+    else 
+        @p= "가"
+    end 
+        
+     erb :test
+ end
+post '/baskinbin' do
+    puts "들어왔습니다."
+        "Get started"
+end
